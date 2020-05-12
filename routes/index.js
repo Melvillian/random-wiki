5e07c08320a6cef7b91e77fa3849048d761316d8
Melvillian <melvillevt@gmail.com>
2020-05-12 12:32:26 -0400

    refactored names and error handling logic

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,11 @@
 var express = require('express');
 var _ = require('lodash');
-const validUrl = require('valid-url');
 var router = express.Router();
 var wiki = require('wikijs').default;
 
 /* GET home page. */
 // TODO generic error handling
-router.get('/', function(req, res, next) {
-  // Cookies that have not been signed
-  console.log('Cookies: ', req.cookies)
-  console.log(JSON.stringify(req.body));
+router.get('/', function (req, res, next) {
 
   if (req.cookies.melville_wiki_cookie) {
     // 1. use cookie to get user.
@@ -23,29 +19,44 @@ router.get('/', function(req, res, next) {
 
 /* POST register new user */
 // TODO generic error handling
-router.post('/register', async function(req, res, next) {
+router.post('/register', async function (req, res, next) {
   try {
-    const wikisite = req.body.wikisite;
-    if (!validUrl.isHttpUri(wikisite) && !validUrl.isHttpsUri(wikisite)) {
-      throw new Error(`need a valid URL for req.body.wikisite: ${wikisite}`);
+    const wikitopic = req.body.wikitopic; // a wikipedia topic, like "Windston Churchill"
+
+    // fetch wikitopic and extract categories.
+    const categories = await getTopicCategories(wikitopic);
+    if (_.isEmpty(categories)) {
+      throw new Error(`could not find an interesting enough wikipedia entry for ${wikitopic}`);
     }
 
-    // fetch wikipage and extract categories
-    const categories = await getPageCategories("Winston Churchill");
     res.redirect('/');
   } catch (error) {
     return next(error)
   }
 });
 
-const getPageCategories = async (pageName) => {
-  const page = await wiki({ headers: { 'User-Agent': 'my-script-name (https://my-script-link; my@email) wiki.js' } }).page(pageName);
+const unwantedCategoryPrefixes = [
+  "Wikipedia",
+  "Articles",
+  "All article disambiguation pages",
+  "All disambiguation pages",
+  "Disambiguation pages",
+  "Disambiguation pages with short description",
+  "Place name disambiguation pages"
+]
+
+/**
+ * fetch a wikitopic's categories and filter down unwanted categories
+ * @param {String} wikitopic
+ */
+const getTopicCategories = async (wikitopic) => {
+  const page = await wiki({ headers: { 'User-Agent': 'my-script-name (https://my-script-link; my@email) wiki.js' } }).page(wikitopic);
   const rawCategories = await page.categories();
   return rawCategories
     // every raw category from wiki looks like "Category:1874 births", so we need to cut out the prefix
     .map((category) => category.substring(9))
-     // remove wikipedia's meta categories we don't like
-    .filter((category) => !_.startsWith(category, "Wikipedia") && !_.startsWith(category, "Articles"));
+    // remove wikipedia's meta categories we don't like
+    .filter((category) => unwantedCategoryPrefixes.every((unwantedCategory) => !_.startsWith(category, unwantedCategory)));
 }
 
 // TODO do this so the session cannot be guesses
